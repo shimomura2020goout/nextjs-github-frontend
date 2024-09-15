7b19016c6ccf1b2cb5bdf536c47efa8b96310abe
shimomurahiroki <69880795+shimomura2020goout@users.noreply.github.com>
2024-09-16 07:10:46 +0900

    0915Edit

--- __tests__/Home.test.js
+++ __tests__/Home.test.js
@@ -2,7 +2,6 @@
  * @jest-environment jsdom
  */
 import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
 import Home from '../pages/index'
 
 it('Should render title text', () => {
