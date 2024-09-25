bcb3c13c87937c6b34e8751fc8995c5cf08e0314
shimomurahiroki <69880795+shimomura2020goout@users.noreply.github.com>
2024-09-26 08:41:17 +0900

    initial commit

--- __tests__/Home.test.js
+++ __tests__/Home.test.js
@@ -1,7 +1,5 @@
-/**
- * @jest-environment jsdom
- */
 import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
 import Home from '../pages/index'
 
 it('Should render title text', () => {
